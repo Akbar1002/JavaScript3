2224f533f6919d9484e55d840c869106ab5bbf13
Akbar <akbar.erken@gmail.com>
2020-12-28 11:52:58 +0100

    fixed ex3

--- Week1/homework/js-exercises/ex3-dogPhotoGallery.js
+++ Week1/homework/js-exercises/ex3-dogPhotoGallery.js
@@ -1,11 +1,4 @@
 const gallery = document.getElementById('gallery');
-const main = document.querySelector('#main');
-const li = document.createElement('li');
-const img = document.createElement('img');
-main.appendChild(li);
-li.appendChild(img);
-img.style.width = '500px';
-img.style.height = '400px';
 
 document.getElementById('click').onclick = function () {
   getInfo();
@@ -16,12 +9,22 @@ document.getElementById('click-axios').onclick = function () {
 
 //XMLHttpRequest
 function getInfo() {
+  const main = document.querySelector('#main');
+  const ul = document.createElement('ul');
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+  main.appendChild(ul);
+  ul.appendChild(li);
+  li.appendChild(img);
+  img.style.width = '300px';
+  img.style.height = '200px';
   const xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
 
   xhr.onload = function () {
     const info = xhr.response;
     img.src = info.message;
+
     if (xhr.status < 400) {
       console.log(info);
     } else {
@@ -46,6 +49,15 @@ function getInfoWithAxios() {
     .get('https://dog.ceo/api/breeds/image/random')
 
     .then(function (response) {
+      const main = document.querySelector('#main');
+      const ul = document.createElement('ul');
+      const li = document.createElement('li');
+      const img = document.createElement('img');
+      main.appendChild(ul);
+      ul.appendChild(li);
+      li.appendChild(img);
+      img.style.width = '300px';
+      img.style.height = '200px';
       img.src = response.data.message;
 
       console.log(response);
