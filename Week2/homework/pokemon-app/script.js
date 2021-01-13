d1b8e8b1cc7c453bb5c3156fb2b7093b764e4f18
Akbar <akbar.erken@gmail.com>
2021-01-13 09:44:02 +0100

    fixed some error

--- Week2/homework/pokemon-app/script.js
+++ Week2/homework/pokemon-app/script.js
@@ -1,7 +1,37 @@
-function fetchData() {}
-fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+const fetchData = (url) => fetch(url);
 
-const btn = document.getElementById('btn');
-const select = document.getElementById('select');
+async function main(url) {
+  const select = document.createElement('select');
+  const options = document.createElement('option');
+  options.innerText = 'Please select';
+  options.value = '';
+  select.appendChild(options);
+  const imgContainer = document.createElement('div');
+  document.body.appendChild(select);
+  document.body.appendChild(imgContainer);
+  const data = await fetchData(url).then((res) => res.json());
+  addP(data.results, select);
+  select.addEventListener('change', async () => {
+    const url = select.value;
+    const response = await fetchData(url).then((res) => res.json());
+    addImg(response.sprites.front_default, imgContainer);
+  });
+}
+
+const addP = (response, target) => {
+  response.forEach((element) => {
+    const option = document.createElement('option');
+    option.innerText = element.name;
+    option.value = element.url;
+    target.appendChild(option);
+  });
+};
+const addImg = (res, target) => {
+  target.innerHTML = null;
+  const img = document.createElement('img');
+  img.src = res;
+  target.appendChild(img);
+};
+
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon';
+window.onload = () => main(baseUrl);
